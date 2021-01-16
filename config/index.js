eb2fa061cf920f71ea950e05d6dd95e7feea0931
jasmjazz <house5324@gmail.com>
2021-01-16 22:11:29 +0800

    0116

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/stray-help/',
+    assetsPublicPath: '/stray-help/dist/',
 
     /**
      * Source Maps
